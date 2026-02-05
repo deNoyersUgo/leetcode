41e31374c302d24280b73e2aa6251310bb67cf95
deNoyersUgo <ugodenoyers@gmail.com>
2026-02-05 14:29:43 +0100

    longest substring without repeating characters is done. median of two sorted arrays to be done

--- longest_substring_without_repeating_numbers/src/main.rs
+++ longest_substring_without_repeating_numbers/src/main.rs
@@ -1,3 +1,31 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut longest: usize = 0;
+        let mut current: Vec<char> = Vec::new();
+
+        for c in s.chars() {
+            if let Some(index) = current.iter().position(|&x| x == c) {
+                longest = std::cmp::max(current.len(), longest);
+
+                for _ in 0..=index {
+                    current.remove(0);
+                }
+            }
+
+            current.push(c);
+        }
+
+        std::cmp::max(current.len(), longest) as i32
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let first_case = Solution::length_of_longest_substring(String::from("abcabcbb"));
+    println!("First case is {}", first_case);
+    let second_case = Solution::length_of_longest_substring(String::from("bbbbb"));
+    println!("First case is {}", second_case);
+    let third_case = Solution::length_of_longest_substring(String::from("pwwkew"));
+    println!("First case is {}", third_case)
 }
---
+++ median_of_two_sorted_arrays/src/main.rs
@@ -0,0 +1,11 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
