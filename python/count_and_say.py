237b4a7552af3e43e882550cd0bcca1176c2019b
deNoyersUgo <ugodenoyers@gmail.com>
2026-04-01 08:56:26 +0200

    go on with python combination sum

---
+++ python/count_and_say.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        res = '1'
+        
+        for _ in range(1,n):
+            prev, count = '.', 0
+            curr_str = ""
+            
+            for char in res:
+                if char == prev or prev == '.':
+                    count += 1
+                else:
+                    curr_str += str(count) + prev
+                    count = 1
+                prev = char
+            
+            curr_str += str(count) + prev
+            res = curr_str
+            
+        return res
+        
+test1 = Solution().countAndSay(1)
+print(f"Output of test1: {test1}\n")
+test2 = Solution().countAndSay(4)
+print(f"Output of test2: {test2}\n")
